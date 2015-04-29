14108a94e12710bbdd199601d86fabaf4aa79a48
Yanghg <yanghong2014@u.northwestern.edu>
2015-04-29 15:56:59 -0500

    haha

--- decisiontree.py
+++ decisiontree.py
@@ -2,22 +2,23 @@ import csv
 
 class TreeNode:
     def __init__(self, examples):
-        self.name = "";
-        self.children = [];
-        name, rule = bestAttribute(examples)
+        self.name = ""
+        self.children = []
+        name, splitRules = bestAttribute(examples)
         self.name = name
-        for x in rule: 
+        for x in splitRules: 
             subExamples = getSubExamples(x)
             subtree = Node(subExamples)
             self.children.append(subtree)
     
     def bestAttribute(self, examples):
-
+        for examples
         return "hehe", []
 
     def getSubExamples(self, examples):
-
+        return 
     def handleAttribute(self, examples, name):
+        return 
 
 def readFile(fileName):
     # "rb" is read only
