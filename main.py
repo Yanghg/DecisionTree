3043689d23c4b0772c617dd8fdd3420cb16a1d08
missfxhr <xiaofang2016@u.northwestern.edu>
2015-04-29 19:33:04 -0500

    solve function added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 from decisiontree import TreeNode
 
-#examples = [["aa","bb","cc"][1 2 3][4 5 6]....[7 8 9]]
+#examples = [["aa","bb","cc"],[True,False,True],[1 2 3],[4 5 6]....[7 8 9]]
 
 
 def handleAttribute(examples, name):
@@ -11,3 +11,11 @@ def readFile(fileName):
     # "rb" is read only
     csvReader = csv.reader(open(fileName,"rb"),delimiter=',') 
     return list(csvReader)
+
+def printTree(root):
+    return 
+
+def solve(fileName):
+    examples = readFile(fileName)
+    root = TreeNode(examples,None)
+    return root
