49225b530e9f82d7bfb6ae241b79644143905393
missfxhr <xiaofang2016@u.northwestern.edu>
2015-04-30 23:10:39 -0500

    main indent fix

--- main.py
+++ main.py
@@ -31,16 +31,16 @@ def readFile(fileName):
         mostcfL.append([-1])
         if temp!= '?':
             if temp.find('.')!=-1:
-            	Dis_Cons.append(False)
+                Dis_Cons.append(False)
             else:
-            	Dis_Cons.append(True)
+                Dis_Cons.append(True)
         else:
             temp=data[2][i]
             if temp!= '?':
-            	if temp.find('.')!=-1:
-            		Dis_Cons.append(False)
-            	else:
-            		Dis_Cons.append(True)
+                if temp.find('.')!=-1:
+                    Dis_Cons.append(False)
+                else:
+                    Dis_Cons.append(True)
     examples.append(data[0][:])
     examples.append(Dis_Cons[:])
     del examples[1][dlength-1]
@@ -49,74 +49,64 @@ def readFile(fileName):
         examples[2].append(False)
     # Turn str to int or float
     for i in range(1,tlength):
-    	examples.append([])
-    	for j in range(0,dlength):
-    		if data[i][j]!='?':
-    			if Dis_Cons[j]==True:
-    				examples[i+2].append(int(data[i][j]))
-    				if data[i][dlength-1].find('1')!=-1:
-    					if mapsW[j].has_key(examples[i+2][j])==True:
-    						mapsW[j][examples[i+2][j]]+=1
-    					else:
-    						mapsW[j][examples[i+2][j]]=1
-    					if mostcfW[j][0]<mapsW[j][examples[i+2][j]] :
+        examples.append([])
+        for j in range(0,dlength):
+            if data[i][j]!='?':
+                if Dis_Cons[j]==True:
+                    examples[i+2].append(int(data[i][j]))
+                    if data[i][dlength-1].find('1')!=-1:
+                        if mapsW[j].has_key(examples[i+2][j])==True:
+                            mapsW[j][examples[i+2][j]]+=1
+                        else:
+                            mapsW[j][examples[i+2][j]]=1
+                        if mostcfW[j][0]<mapsW[j][examples[i+2][j]] :
+                            if len(mostcfW[j])==1:
+                                mostcfW[j].append(examples[i+2][j])
+                            else:
+                                mostcfW[j][0]=mapsW[j][examples[i+2][j]]
+                                mostcfW[j][1]=examples[i+2][j]
+                    else:
+                        if mapsL[j].has_key(examples[i+2][j])==True:
+                            mapsL[j][examples[i+2][j]]+=1
+                        else:
+                            mapsL[j][examples[i+2][j]]=1
+                        if mostcfL[j][0]<mapsL[j][examples[i+2][j]] :
+                            if len(mostcfL[j])==1:
+                                mostcfL[j].append(examples[i+2][j])
+                            else:
+                                mostcfL[j][0]=mapsL[j][examples[i+2][j]]
+                                mostcfL[j][1]=examples[i+2][j]
+                else:
+                    examples[i+2].append(float(data[i][j]))
+                    if data[i][dlength-1].find('1')!=-1:
+                        if mapsW[j].has_key(examples[i+2][j])==True:
+                            mapsW[j][examples[i+2][j]]+=1
+                        else:
+                            mapsW[j][examples[i+2][j]]=1
+                        if mostcfW[j][0]<mapsW[j][examples[i+2][j]] :
+                            if len(mostcfW[j])==1:
+                                mostcfW[j].append(examples[i+2][j])
+                            else:
+                                mostcfW[j][0]=mapsW[j][examples[i+2][j]]
+                                mostcfW[j][1]=examples[i+2][j]	
+                    else:
+                        if mapsL[j].has_key(examples[i+2][j])==True:
+                            mapsL[j][examples[i+2][j]]+=1
+                        else:
+                            mapsL[j][examples[i+2][j]]=1
+                        if mostcfL[j][0]<mapsL[j][examples[i+2][j]] :
 
-
-						if len(mostcfW[j])==1:
-							mostcfW[j].append(examples[i+2][j])
-						else:
-    							mostcfW[j][0]=mapsW[j][examples[i+2][j]]
-    							mostcfW[j][1]=examples[i+2][j]
-    				else:
-    					if mapsL[j].has_key(examples[i+2][j])==True:
-    						mapsL[j][examples[i+2][j]]+=1
-    					else:
-    						mapsL[j][examples[i+2][j]]=1
-    					if mostcfL[j][0]<mapsL[j][examples[i+2][j]] :
-
-						if len(mostcfL[j])==1:
-							mostcfL[j].append(examples[i+2][j])
-						else:
-    							mostcfL[j][0]=mapsL[j][examples[i+2][j]]
-    							mostcfL[j][1]=examples[i+2][j]
-
-    			else:
-    				examples[i+2].append(float(data[i][j]))
-    				if data[i][dlength-1].find('1')!=-1:
-    					if mapsW[j].has_key(examples[i+2][j])==True:
-    						mapsW[j][examples[i+2][j]]+=1
-    					else:
-    						mapsW[j][examples[i+2][j]]=1
-    					if mostcfW[j][0]<mapsW[j][examples[i+2][j]] :
-
-						if len(mostcfW[j])==1:
-							mostcfW[j].append(examples[i+2][j])
-						else:
-
-    							mostcfW[j][0]=mapsW[j][examples[i+2][j]]
-    							mostcfW[j][1]=examples[i+2][j]	
-
-    				else:
-    					if mapsL[j].has_key(examples[i+2][j])==True:
-    						mapsL[j][examples[i+2][j]]+=1
-    					else:
-    						mapsL[j][examples[i+2][j]]=1
-    					if mostcfL[j][0]<mapsL[j][examples[i+2][j]] :
-
-						if len(mostcfL[j])==1:
-							mostcfL[j].append(examples[i+2][j])
-						else:
-
-    							mostcfL[j][0]=mapsL[j][examples[i+2][j]]
-    							mostcfL[j][1]=examples[i+2][j]
-
-
-    		else:
-                	if j==dlength-1:
-                    		delation.append(i+2)
-                	else:
-    			    	alignpos.append([i,j])
-    			     	examples[i+2].append(-50)
+                            if len(mostcfL[j])==1:
+                                mostcfL[j].append(examples[i+2][j])
+                            else:
+                                mostcfL[j][0]=mapsL[j][examples[i+2][j]]
+                                mostcfL[j][1]=examples[i+2][j]
+            else:
+                if j==dlength-1:
+                    delation.append(i+2)
+                else:
+                    alignpos.append([i,j])
+                    examples[i+2].append(-50)
     # Switch ? to the most common data
     for cc in range(0,len(alignpos)):
         a=alignpos[cc][0]
@@ -127,8 +117,8 @@ def readFile(fileName):
             examples[a+2][b]=mostcfL[b][1]
     counterz=0
     for i in range(0,len(delation)):
-	del examples[delation[i]-counterz]
-	counterz+=1
+        del examples[delation[i]-counterz]
+        counterz+=1
     return examples
 
 def printTree(root):
